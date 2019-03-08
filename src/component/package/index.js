c7d37f0ed2479ace89941bb9e1083232e1aca467
dutao <1173127007@qq.com>
2019-03-08 11:19:23 +0800

    feat: 学习text-stroke

--- src/component/package/index.js
+++ src/component/package/index.js
@@ -1,11 +1,11 @@
 import React, { PureComponent } from 'react';
-import scss from './index.scss';
+import scss from './index.module.scss';
 
 function outerPackage (Component, title) {
   return class Package extends PureComponent {
     render () {
       return (
-        <div className={scss['packages']}>
+        <div className={scss['package']}>
           <h2>{title}</h2>
           <Component {...this.props}/>
         </div>
--- src/modules/textStroke/UI.index.js
+++ src/modules/textStroke/UI.index.js
@@ -1,14 +1,17 @@
 import React, { PureComponent } from 'react';
 import outerPackage from 'src/component/package';
+const scss = require('./index.module.scss');
 
 class TextStroke extends PureComponent {
   render () {
     return (
-      <div>
-        <span>Css-Effect</span>
+      <div className={scss['stroke']}>
+        <span>-webkit-text-stroke</span>
+        <span>webkit-text-stroke</span>
+        <span>webkit-text-stroke</span>
       </div>
     )
   }
 };
 
-export default outerPackage(TextStroke, 'test-Stroke');
+export default outerPackage(TextStroke, 'text-stroke');
